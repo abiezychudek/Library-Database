902a109c65d272fee282523f2f07427567ccc68d
Dora455 <91740412+Dora455@users.noreply.github.com>
2022-12-22 17:05:20 +0100

    Update views.sql

--- views.sql
+++ views.sql
@@ -1,9 +1,11 @@
-CREATE VIEW [Staff sector] AS
-SELECT St.name, St.surname
+
+CREATE OR REPLACE VIEW  staff_floor AS
+SELECT St.name, St.surname, SE.floor
 FROM STAFF St, SECTOR Se
 WHERE St.sector_id = Se.sector_id ;
 
-CREATE VIEW [Best Books] AS
+
+CREATE OR REPLACE VIEW best_books AS
 SELECT T.PLACE, B.title, C.CATEGORY_name
 FROM BOOK B, CATEGORY C, TOP_BOOKS T
 WHERE B.book_id = T.BOOK_ID AND B.category_id = C.CATEGORY_ID;
