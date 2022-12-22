3783e4d40c9804f3acf09218bf9b7a6ce83e2d78
Dora455 <91740412+Dora455@users.noreply.github.com>
2022-12-22 17:11:45 +0100

    Update views.sql

--- views.sql
+++ views.sql
@@ -9,3 +9,10 @@ CREATE OR REPLACE VIEW best_books AS
 SELECT T.PLACE, B.title, C.CATEGORY_name
 FROM BOOK B, CATEGORY C, TOP_BOOKS T
 WHERE B.book_id = T.BOOK_ID AND B.category_id = C.CATEGORY_ID;
+
+CREATE OR REPLACE VIEW  author_books AS
+SELECT A.name, A.surname, B.title
+FROM author A, book B
+WHERE A.author_id = B.author_id 
+ORDER BY A.name;
+
