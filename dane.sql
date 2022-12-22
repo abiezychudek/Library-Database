c7a430fb4e8393a7775e449322d4e05069e245c4
Dora455 <91740412+Dora455@users.noreply.github.com>
2022-12-22 16:26:03 +0100

    Update dane.sql

--- dane.sql
+++ dane.sql
@@ -10,6 +10,16 @@ INSERT INTO author VALUES
 (9,'Anna','Lewandowska'),
 (10,'Kylian','Mbappe')
 
+INSERT INTO SECTOR VALUES
+(1, 0, 8, 3),
+(2, 0, 8, 3),
+(3, 0, 6, 4),
+(4, 0, 2, 6),
+(5, 1, 4, 4),
+(6, 1, 5, 2),
+(7, 1, 4, 0),
+(8, 1, 3, 4)
+
 INSERT INTO category VALUES
 (1,'Fantasy'),
 (2,'Horror'),
@@ -51,7 +61,25 @@ INSERT INTO book VALUES
 (11,9,'Koziolek Matolek',1,'2008-09-09',1,FALSE),
 (12,10,'Koziolek Matolek',1,'2012-10-17',1,TRUE),
 (13,1,'Koziolek Matolek',1,'2017-01-21',1,TRUE),
-(14,2,'Koziolek Matolek',1,'2011-12-11',1,FALSE)
+(14,2,'Koziolek Matolek',1,'2011-12-11',1,FALSE),
+(15,2,'Don Kichot', 5,'1999-01-20',8,TRUE),
+(16,12,'Duma i uprzedzenie',10,' 1978-05-17',8,TRUE),
+(17,10,'Hrabia Monte Christo', 8,'2004-09-25',7,FALSE),
+(18,9,'Lalka', 9,'2002-08-30',4,TRUE),
+(19,7,'Piata Gora', 7,'2008-02-22',7,FALSE),
+(20,4,'Cien Ojca', 6,'2012-01-01',5,FALSE),
+(21,11,'Swiat Zofii', 5,'2010-10-02',8,FALSE),
+(22,6,'Trzech Muszkieterow', 1,'2019-12-18',6,FALSE),
+(23,7,'Sherlock Holmes', 4,'1967-06-13',5,TRUE),
+(24,8,'Hobbit', 3,'1959-04-16',2,TRUE),
+(25,1,'Wladca Pierscieni', 8,'2000-11-10',3,FALSE),
+(26,9,'Imie Rozy', 4,'2001-09-01',5,TRUE),
+(27,6,'Zabawy Poufne', 9,'2009-11-29',1,FALSE),
+(28,4,'Faust', 10,'2018-12-23',4,FALSE),
+(29,9,'Sen nocy letniej', 3,'2022-01-06',7,TRUE),
+(30,12,'Poskromienie zlosnicy', 1,'2001-02-19',6,TRUE),
+(31,11,'Ksiaze', 2,'1999-04-12',5,FALSE),
+
 
 INSERT INTO publisher VALUES
 (1,'HarperCollins'),
@@ -97,3 +125,15 @@ INSERT INTO member_status VALUES
 (10,3,5),
 (11,2,5),
 (12,5,5)
+
+INSERT INTO TOP_BOOKS VALUES
+(1,19),
+(2,27),
+(3,8),
+(4,14),
+(5,3),
+(6,30),
+(7,20),
+(8,10),
+(9,17),
+(10,10)
