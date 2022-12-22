1064007a872cc7f82deadf5dc59b4e91278fc115
Dora455 <91740412+Dora455@users.noreply.github.com>
2022-12-22 17:12:49 +0100

    Update views.sql

--- views.sql
+++ views.sql
@@ -10,7 +10,7 @@ SELECT T.PLACE, B.title, C.CATEGORY_name
 FROM BOOK B, CATEGORY C, TOP_BOOKS T
 WHERE B.book_id = T.BOOK_ID AND B.category_id = C.CATEGORY_ID;
 
-CREATE OR REPLACE VIEW  author_books AS
+CREATE OR REPLACE VIEW  authors_books AS
 SELECT A.name, A.surname, B.title
 FROM author A, book B
 WHERE A.author_id = B.author_id 
