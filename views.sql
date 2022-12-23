4b6d93f393684d574a89c3526f6fb1af98c1a14b
Alabi <Ala.bie79@interia.pl>
2022-12-23 15:12:20 +0100

    5 widok

--- views.sql
+++ views.sql
@@ -27,3 +27,24 @@ JOIN loan_books d ON d.member_id = m.member_id
 WHERE (DATE_PART('year', CURRENT_DATE) - DATE_PART('year', return_data::date)
 +DATE_PART('month', CURRENT_DATE) - DATE_PART('month', return_data::date)
 +DATE_PART('day', CURRENT_DATE) - DATE_PART('day', return_data::date))>0
+
+--5 Widok osoby wraz z tytułem i kategoria książki na jaki oczekuje oraz pełna nazwa osoby, która ją prztrzymuje
+SELECT T.Full_name_booker,T.title,C.category_name ,loan.Full_name_loaner
+	FROM(
+		SELECT 
+			b.title, b.book_id,b.category_id,
+			m.name || ' ' || m.surname AS Full_name_booker
+		FROM member	m
+		JOIN reservation ON reservation.member_id = m.member_id
+		JOIN book  b ON b.book_id = reservation.book_id ) T
+	
+	JOIN (
+		SELECT 
+			mem.name || ' ' || mem.surname AS Full_name_loaner,
+			loan_books.book_id
+		FROM  member mem
+		JOIN loan_books ON loan_books.member_id = mem.member_id
+	) loan  ON loan.book_id= T.book_id
+	
+	JOIN category C ON C.category_id=T.category_id
+
