2e8afd29763863e88619c88cd49708d16b7d5e30
Alabi <Ala.bie79@interia.pl>
2023-01-02 17:39:10 +0100

    2triggery

--- triggersy.sql
+++ triggersy.sql
@@ -17,3 +17,22 @@ CREATE TRIGGER return_book
 	EXECUTE PROCEDURE log_last_name_changes();
 	
 	
+CREATE OR REPLACE FUNCTION too_many_books()
+  RETURNS TRIGGER 
+  LANGUAGE PLPGSQL
+AS
+ $$
+BEGIN 
+IF (SELECT currently_loan FROM member_status WHERE  NEW.member_id=member_id)= ((SELECT max_to_loan FROM member_status WHERE NEW.member_id=member_id))
+ THEN  
+ 	RAISE NOTICE 'You have the limit';
+	return null;
+	END IF;
+	return NEW;
+	END; $$;
+
+	
+CREATE TRIGGER check_valid_loan_book
+  BEFORE INSERT ON loan_books
+FOR EACH ROW EXECUTE PROCEDURE   too_many_books();
+
